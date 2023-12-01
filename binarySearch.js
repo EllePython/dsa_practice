44d53b0d1d477082fc6be732a975ab91afb949a6
Eliana_Lugo <Ellelugo91@gmail.com>
2023-12-01 15:21:47 -0500

    solution by Lorena

--- binarySearch.js
+++ binarySearch.js
@@ -6,3 +6,25 @@
 // I am to create a function to search if the target exist in the array, if the target exist it will return the index
 // if not it will return -1
 
+// Binary search divides the search space in half until the target value is found
+
+var search = function(nums, target) {
+    let left = O;
+    let right = nums.length-1;
+
+    while(left <= right){
+        const mid = Math.floor((left+right)/2);
+
+        if(nums[mid]=== target){
+            return mid;
+        }
+
+        if(nums[mid]<target){
+            left = mid+1
+        }else{
+            right= mid-1
+        }
+        
+    }
+    return -1;
+};
