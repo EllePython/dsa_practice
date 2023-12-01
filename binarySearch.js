128492b201733f61244dcb738f3ce7e32c56e08a
Eliana_Lugo <Ellelugo91@gmail.com>
2023-12-01 14:42:14 -0500

    pesudocode the problem/binary search

---
+++ binarySearch.js
@@ -0,0 +1,8 @@
+// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target
+// in nums. If target exists, then return its index. Otherwise, return -1.
+// You must write an algorithm with O(log n) runtime complexity.
+
+// pseudocode problem: In a array that is sorted in an ascending form of integers there is a target that exist
+// I am to create a function to search if the target exist in the array, if the target exist it will return the index
+// if not it will return -1
+
