37f7345a2f8eb41661ff1b69809ed80e39ecf1e9
Eliana_Lugo <Ellelugo91@gmail.com>
2023-12-14 16:00:13 -0500

    added first test & updated package.json

---
+++ binaryTree.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/explore/featured/card/top-interview-questions-easy/94/trees/555/
+
+// Pseudocode problem: You have the root of the tree what is the depth/length of the longest node
+
+var maxDepth = function(root) {
+
+};
+
+module.exports = maxDepth;
---
+++ binaryTree.test.js
@@ -0,0 +1,14 @@
+const maxDepth = require("./binaryTree")
+
+// test the farthest leaf node 
+
+
+// test something
+// test('test something',() => {
+//     expect(1).toBe(true);
+// })
+
+test('should create an empty tree', () => {
+    const bst = this.root = null;
+    expect(bst).toBe(null);
+})
