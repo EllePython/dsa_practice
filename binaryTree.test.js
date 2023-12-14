cc079eaa2c361400c5160800bd5ef16be2416bc8
Eliana_Lugo <Ellelugo91@gmail.com>
2023-12-14 17:07:47 -0500

    added testing and cleaned code

--- binaryTree.test.js
+++ binaryTree.test.js
@@ -11,4 +11,15 @@ const maxDepth = require("./binaryTree")
 test('should create an empty tree', () => {
     const bst = this.root = null;
     expect(bst).toBe(null);
+
+
+})
+
+test('should return maximum depth', () => {
+    const bst = this.root = 1;
+    const leftbst = this.root.left = 2;
+    const rightbst = this.root.right = 3;
+    // const leftbst2 = this.root.left.left = 4;
+    // const rightbst2 = this.root.right.right = 5;
+    expect(maxDepth).toBe(3);
 })
